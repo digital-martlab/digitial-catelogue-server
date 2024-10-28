34c9f6ec4322c62e63fbec08f3ed190b7f40676c
vaibhav-xp <vibhu.astute@gmail.com>
2024-10-28 22:00:56 +0530

    specified constants

--- config/constant-variables.js
+++ config/constant-variables.js
@@ -3,14 +3,14 @@ const dotenv = require("dotenv")
 dotenv.config();
 
 module.exports = {
-    DB_HOST: process.env.DB_HOST,
-    DB_USER: process.env.DB_USER,
-    DB_PASSWORD: process.env.DB_PASSWORD,
-    DB_NAME: process.env.DB_NAME,
+    DB_HOST: "localhost",
+    DB_USER: "root",
+    DB_PASSWORD: "root",
+    DB_NAME: "digital_catelogue_app",
     PORT: 3001,
-    JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
+    JWT_SECRET_KEY: "$2b$12$Rzxx6pFwVvDmYVTMPCzQeeU1/seWXap.kKMQJGYZMA4KypY5s/lvq",
     SALT: 12,
-    CLOUDINARY_NAME: process.env.CLOUDINARY_NAME,
-    CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
-    CLOUDINARY_SECRET_KEY: process.env.CLOUDINARY_SECRET_KEY,
+    CLOUDINARY_NAME: "dipgig7oy",
+    CLOUDINARY_API_KEY: "342668552755793",
+    CLOUDINARY_SECRET_KEY: "AgBtOs9pw9-LFQuYeeP-G_qLgbw",
 }
