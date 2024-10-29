f9bee44cdcaff3a7f6641d3cf7f14f94ca880b67
vaibhav-xp <vibhu.astute@gmail.com>
2024-10-29 08:29:12 +0530

    added db credential

--- config/constant-variables.js
+++ config/constant-variables.js
@@ -3,9 +3,9 @@ const dotenv = require("dotenv")
 dotenv.config();
 
 module.exports = {
-    DB_HOST: "localhost",
-    DB_USER: "root",
-    DB_PASSWORD: "root",
+    DB_HOST: "35.154.28.45",
+    DB_USER: "new-db",
+    DB_PASSWORD: "123@123@123",
     DB_NAME: "digital_catelogue_app",
     PORT: 3001,
     JWT_SECRET_KEY: "$2b$12$Rzxx6pFwVvDmYVTMPCzQeeU1/seWXap.kKMQJGYZMA4KypY5s/lvq",
