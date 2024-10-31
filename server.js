49fefd94375e36799a473243593313169eeba7d1
vaibhav-xp <vibhu.astute@gmail.com>
2024-10-31 12:07:01 +0530

    removed console.log

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const app = express();
 
 // Middleware
 app.use(helmet())
-// app.use(cors({ origin: allowedOriginMiddlware }));
+app.use(cors({ origin: allowedOriginMiddlware }));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
